752048ace688c9db695a614290afaab1027d7a00
HooLo <hoolohi@gmail.com>
2022-01-09 20:08:15 +0200

    home work 5

--- src/HomeWork5/Game.java
+++ src/HomeWork5/Game.java
@@ -1,2 +1,12 @@
-package HomeWork5;public class Game {
+package HomeWork5;
+
+public class Game {
+    public static void main(String[] args){
+        Poker poker = new Poker();
+
+        Card[] deck = poker.createDeck();
+        poker.shuffle(deck);
+        poker.serveCards(deck, 4);
+
+    }
 }
